ff5f80668285340df08257275ca5f16c4d0b750d
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-19 22:40:40 +0200

    add slick slider
    add slick slider

--- olgaphoto/js/script.js
+++ olgaphoto/js/script.js
@@ -1,29 +1,23 @@
-var writeToUsBtn = document.querySelector(".btn-adress");
-var modalContactBlock = document.querySelector(".modal-contact-block");
-var closeModalBlock = document.querySelector(".close-modal-block");
-var map = document.querySelector(".map");
-var modalMap = document.querySelector(".modal-map");
-var closeModalMap = document.querySelector(".close-modal-map");
-var popupOverlay = document.querySelector(".popup-overlay");
-var contactForm = document.querySelector(".contact-form");
-var name = document.querySelector("[name=name]");
-var email = document.querySelector("[name=email");
+$(document).ready(function(){
+  var h_hght = $(".header").outerHeight();
+  var h_nav = $(".menu").outerHeight();
+  var top;
+  $(window).scroll(function(){
+    top = $(this).scrollTop();
+    if((h_hght-top) <= h_nav){
+      $(".menu").css("top","0");
+    }
+    else if(top < h_hght && top > 0){
+      $(".menu").css({"bottom" : top, "top":""});
+    }
+    else if(top < h_nav){
+      $(".menu").css({"top":"","bottom":"-95px"});
+    }
+  });
+});
 
-	writeToUsBtn.addEventListener("click", function(event) {
-		event.preventDefault();
-		modalContactBlock.classList.add("modal-contact-block-open");
-		popupOverlay.classList.add("popup-overlay-open");
-	});
-	
-	closeModalBlock.addEventListener("click", function(event) {
-		event.preventDefault();
-		modalContactBlock.classList.remove("modal-contact-block-open");
-		popupOverlay.classList.remove("popup-overlay-open");
-	});
-
-	contactForm.addEventListener("submit", function(event) {
-		if(!name || !email) {
-			event.preventDefault();
-			alert("ВЫ НЕ ВВЕЛИ ДАННЫЕ");
-		}
-	});	
+$(document).ready(function(){
+  $(".slider").slick({
+    dots: true
+  });
+});
