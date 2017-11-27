f44f9cd2c1d5df30b7492a52131243cf3b535600
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-27 10:13:35 +0200

    bar2

--- hpm/js/script.js
+++ hpm/js/script.js
@@ -18,21 +18,21 @@ $(document).ready(function(){
 	$(".menu-mob a").click(function() {
 		$(".menu-mob").css("display","none");
 	});
+	
+	function load() {
+    hideAddressBar();
+    //  1
+    //window.addEventListener("orientationchange", function () {
+    //    hideAddressBar();
+    //});
+}
+
+	function hideAddressBar() {
+    if (navigator.userAgent.match(/Android/i) != null) {
+        //window.orientation 0 - 180 - landscape; 90 and -90 portrait
+        document.documentElement.style.height = window.outerHeight + 'px';
+        setTimeout(window.scrollTo(0, 1), 0);
+    }
+}
 });
 
-window.onload = function () {
-    hideAddressBar();
-    window.addEventListener("orientationchange", function () {
-        hideAddressBar();
-    }, false);
-}
-
-function hideAddressBar() {
-    setTimeout(function () {
-        document.body.style.height = window.outerHeight + 'px';
-        setTimeout(function () {
-            window.scrollTo(0, 1);
-        }, 1100);
-    }, 1000);
-    return false;
-}
