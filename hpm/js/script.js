4f21833e00f03310ebda7d5434d031bae1096503
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-27 07:14:25 +0200

    2111170714

--- hpm/js/script.js
+++ hpm/js/script.js
@@ -1,31 +1,9 @@
 $(document).ready(function(){
-  var h_hght = $(".header").outerHeight();
-  var h_nav = $(".menu").outerHeight();
-  var top;
-  $(window).scroll(function(){
-    top = $(this).scrollTop();
-    if((h_hght-top) <= h_nav){
-      $(".menu").css("top","0");
-    }
-    else if(top < h_hght && top > 0){
-      $(".menu").css({"bottom" : top, "top":""});
-    }
-    else if(top < h_nav){
-      $(".menu").css({"top":"","bottom":"-95px"});
-    }
-  });
-	
-	$(".main-menu").on("click", "a", function(){
-		if ($(".main-menu a").hasClass("active")) {
-			$(".main-menu a").removeClass("active");
+	$(".main-header__menu").on("click", "a", function(){
+		if ($(".main-header__menu a").hasClass("active")) {
+			$(".main-header__menu a").removeClass("active");
 		}
 		$(this).addClass("active");
-	});
-	
-	$(".main-menu").on("click", ".to-top", function(){
-		if ($(".main-menu a").hasClass("active")) {
-			$(".main-menu a").removeClass("active");
-		}
 	});
 	
   $(".main-slider").slick({
