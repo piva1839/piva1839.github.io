9be704aef217b01a73c7edbfb142c5c601dd66dd
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-27 15:17:54 +0200

    ...

--- hpm/js/script.js
+++ hpm/js/script.js
@@ -1,3 +1,20 @@
+window.onload = function () {
+    hideAddressBar();
+    window.addEventListener("orientationchange", function () {
+        hideAddressBar();
+    }, false);
+}
+
+function hideAddressBar() {
+    setTimeout(function () {
+        document.body.style.height = window.outerHeight + 'px';
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 1100);
+    }, 1000);
+    return false;
+}
+
 $(document).ready(function(){
 	$(".main-header__menu").on("click", "a", function(){
 		if ($(".main-header__menu a").hasClass("active")) {
@@ -18,7 +35,4 @@ $(document).ready(function(){
 	$(".menu-mob a").click(function() {
 		$(".menu-mob").css("display","none");
 	});
-	
-	var page_size_check = null, q_body; (q_body = $('#body')).bind('DOMSubtreeModified', function() { if (page_size_check === null) { return; } page_size_check = setTimeout(function() { q_body.css('height', ''); if (q_body.height() < window.innerHeight) { q_body.css('height', window.innerHeight + 'px'); } if (!(window.pageYOffset > 1)) { window.scrollTo(0, 1); } page_size_check = null; }, 400); });
 });
-
