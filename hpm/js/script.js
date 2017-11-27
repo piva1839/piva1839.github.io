f9ed90c24b1b08c7a172c5533009d48707a8ab1f
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-27 10:34:19 +0200

    ...

--- hpm/js/script.js
+++ hpm/js/script.js
@@ -19,6 +19,6 @@ $(document).ready(function(){
 		$(".menu-mob").css("display","none");
 	});
 	
-	window.addEventListener("load", function() { window. scrollTo(0, 0); });
+	$(document).ready(function() { if (navigator.userAgent.match(/Android/i)) { window.scrollTo(0,0); var nPageH = $(document).height(); var nViewH = window.outerHeight; if (nViewH > nPageH) { nViewH -= 250; $('BODY').css('height',nViewH + 'px'); } window.scrollTo(0,1); } }); 
 });
 
