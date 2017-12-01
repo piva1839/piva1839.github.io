2fd85a3454052f19f41106c18db7896a8fd27ffd
Oleksii Oboianskyi <piva1839@gmail.com>
2017-12-01 23:58:40 +0200

    hpm_flex

---
+++ hpm_flex/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	$(".main-header__menu").on("click", "a", function(){
+		if ($(".main-header__menu a").hasClass("active")) {
+			$(".main-header__menu a").removeClass("active");
+		}
+		$(this).addClass("active");
+	});
+	
+  $(".main-slider").slick({
+    dots: true,
+    dotsClass: "my-dots",
+  });
+  
+  var headerheight = $(".main-header").height();
+  var addressbarheight = $(".address-bar").height();
+  var totalheight = headerheight + addressbarheight;
+  var mainsliderimgheight = $(window).height() - totalheight+"px";
+  $(".main-slider__img").css("height",mainsliderimgheight);
+  $(window).resize(function(){
+    location.reload();
+  });
+	
+	$(".more-menu-items").click(function(event) {
+		event.preventDefault();
+  });
+  
+  $(".more-menu-items").mouseover(function() {
+    $(".subitems").css("display","block");
+  });
+  
+  $(".subitems").mouseover(function() {
+    $(".subitems").css("display","block");
+  });
+  
+  $(".more-menu-items").mouseout(function() {
+    $(".subitems").css("display","none");
+  });
+  
+  $(".subitems").mouseout(function() {
+    $(".subitems").css("display","none");
+  });
+	
+	$(".subitems__item a").click(function() {
+		$(".subitems").css("display","none");
+	});
+	
+	$(".burger").click(function() {
+		$(".menu-mob").css("display","block");
+	});
+	
+	$(".menu-mob a").click(function() {
+		$(".menu-mob").css("display","none");
+	});
+	
+	$(".fixed-mail").click(function() {
+    $(".modal-contact-block").css("display","block");
+  });
+  
+  $(".close-modal-block").click(function() {
+    $(".modal-contact-block").css("display","none");
+  });
+});
