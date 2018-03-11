6c31147d7bf9b3a67c3ccf50b02837b8c769bd69
Oleksii Oboianskyi <piva1839@gmail.com>
2018-03-11 21:03:36 +0200

    wanthouse
    wanthouse

---
+++ wanthouse/js/script.js
@@ -0,0 +1,14 @@
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+
+$(document).ready(function(){
+  $('.slider-phone').slick({
+    arrows: false,
+    dots: true
+  });
+});
