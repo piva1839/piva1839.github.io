b9580fc6f795d8df6da7ffe227a515e4ee849656
Oleksii Oboianskyi <piva1839@gmail.com>
2018-03-14 10:18:04 +0200

    pre-final

--- wanthouse/js/script.js
+++ wanthouse/js/script.js
@@ -12,3 +12,23 @@ $(document).ready(function(){
     dots: true
   });
 });
+
+$(".calculate-modal").click(function(event){
+    event.preventDefault();
+});
+
+$(".calculate-modal").click(function() {
+  $(".calculate").css("display","block");
+});
+
+$(".modal-close").click(function() {
+  $(".calculate").css("display","none");
+});
+
+$(".order-modal").click(function() {
+  $(".order").css("display","block");
+});
+
+$(".modal-close").click(function() {
+  $(".order").css("display","none");
+});
